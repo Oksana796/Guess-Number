ef9c6cd8ba983f0d908ed06577af266cb901cb56
Oksana <ksenia79460@gmail.com>
2022-03-23 09:58:25 +0000

    refactor reset f

--- script.js
+++ script.js
@@ -42,8 +42,7 @@ checkBtn.addEventListener("click", function () {
 });
 
 //reset
-let reset = againBtn.addEventListener("click", () => {
-  console.log("reset");
+againBtn.addEventListener("click", function () {
   scoreNumber = 20;
   secretNumber = Math.floor(Math.random() * 20 + 1);
   message.textContent = "Start guessing...";
